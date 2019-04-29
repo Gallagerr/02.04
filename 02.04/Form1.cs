e39ddc93f1777d858a731cdd282ad2196a9e3473
sadiev <sadiev@itstep.kz>
2019-04-29 18:22:23 +0600

    Добавьте файлы проекта.

---
+++ 02.04/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02._04
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+      {
+        PersistSecurityInfo = true,
+        UserID = textBox1.Text,
+        Password = textBox2.Text
+      };
+
+      using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+      {
+        try
+        {
+          connection.Open();
+          MessageBox.Show("Connection successful", "Connection", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        catch (SqlException exception)
+        {
+          MessageBox.Show("Connection failed", "Connection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+      }
+    }
+
+    private void button2_Click(object sender, EventArgs e)
+    {
+      Environment.Exit(0);
+    }
+  }
+}
